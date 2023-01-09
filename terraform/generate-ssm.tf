4717eea283a178f2f5c2953d053dcd569744c667
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-01-09 17:26:43 -0500

    Added parameters to track IDL licensing

---
+++ terraform/generate-ssm.tf
@@ -0,0 +1,28 @@
+# SSM Parameter Store parameters for IDL idlenses
+# MODIS Aqua
+resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_aqua" {
+  name = "${var.prefix}-idl-aqua"
+  type = "String"
+  value = "4"
+}
+
+# MODIS Terra
+resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_terra" {
+  name = "${var.prefix}-idl-terra"
+  type = "String"
+  value = "4"
+}
+
+# VIIRS
+resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_viirs" {
+  name = "${var.prefix}-idl-viirs"
+  type = "String"
+  value = "4"
+}
+
+# Retrieval indicator
+resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_ret" {
+  name = "${var.prefix}-idl-retrieving_license"
+  type = "String"
+  value = "False"
+}
