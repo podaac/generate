c82e49b12f767d8b6aad5ca49584785fda974f54
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-01-23 12:44:51 -0500

    Fix formatting

--- terraform/generate-ssm.tf
+++ terraform/generate-ssm.tf
@@ -1,35 +1,35 @@
 # SSM Parameter Store parameters for IDL idlenses
 # MODIS Aqua
 resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_aqua" {
-  name = "${var.prefix}-idl-aqua"
-  type = "String"
+  name  = "${var.prefix}-idl-aqua"
+  type  = "String"
   value = "4"
 }
 
 # MODIS Terra
 resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_terra" {
-  name = "${var.prefix}-idl-terra"
-  type = "String"
+  name  = "${var.prefix}-idl-terra"
+  type  = "String"
   value = "4"
 }
 
 # VIIRS
 resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_viirs" {
-  name = "${var.prefix}-idl-viirs"
-  type = "String"
+  name  = "${var.prefix}-idl-viirs"
+  type  = "String"
   value = "4"
 }
 
 # Floating
 resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_floating" {
-  name = "${var.prefix}-idl-floating"
-  type = "String"
+  name  = "${var.prefix}-idl-floating"
+  type  = "String"
   value = "4"
 }
 
 # Retrieval indicator
 resource "aws_ssm_parameter" "aws_ssm_parameter_ps_idl_ret" {
-  name = "${var.prefix}-idl-retrieving_license"
-  type = "String"
+  name  = "${var.prefix}-idl-retrieving-license"
+  type  = "String"
   value = "False"
 }
