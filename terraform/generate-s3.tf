5850d6b18b5e6add937a845ab4cdcb5c3cb73f0f
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-03-16 14:32:48 +0000

    Add download lists S3 bucket to shared infrastructure

--- terraform/generate-s3.tf
+++ terraform/generate-s3.tf
@@ -28,3 +28,34 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_bucket_fin
     }
   }
 }
+
+# Bucket to hold download lists
+resource "aws_s3_bucket" "aws_s3_bucket_dlc" {
+  bucket = "${var.prefix}-download-lists"
+  tags   = { Name = "${var.prefix}-download-lists" }
+}
+
+resource "aws_s3_bucket_public_access_block" "aws_s3_bucket_dlc_public_block" {
+  bucket                  = aws_s3_bucket.aws_s3_bucket_dlc.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_dlc_ownership" {
+  bucket = aws_s3_bucket.aws_s3_bucket_dlc.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_bucket_dlc_encryption" {
+  bucket = aws_s3_bucket.aws_s3_bucket_dlc.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = "aws/s3"
+    }
+  }
+}
