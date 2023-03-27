75d35376c4e7e958c736a8a4922dc83cfe2528c5
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-03-27 19:08:12 +0000

    Use SSE-s3 encryption key for l2p bucket

--- terraform/generate-s3.tf
+++ terraform/generate-s3.tf
@@ -23,8 +23,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_bucket_fin
   bucket = aws_s3_bucket.aws_s3_bucket_final_granules.bucket
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = "aws/s3"
+      sse_algorithm     = "AES256"
     }
   }
 }
