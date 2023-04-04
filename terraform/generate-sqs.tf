e21f53ce70b8702963066d552d22d7b1ba746c08
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-04-04 19:32:45 +0000

    Switch to FIFO queues organized by dataset

--- terraform/generate-sqs.tf
+++ terraform/generate-sqs.tf
@@ -1,7 +1,7 @@
 # Download lists
 resource "aws_sqs_queue" "aws_sqs_queue_dlc" {
   name                       = "${var.prefix}-download-lists"
-  visibility_timeout_seconds = 60
+  visibility_timeout_seconds = 20
   sqs_managed_sse_enabled    = true
 }
 
@@ -27,14 +27,16 @@ resource "aws_sqs_queue_policy" "aws_sqs_queue_policy_dlc" {
 }
 
 # Pending jobs
-resource "aws_sqs_queue" "aws_sqs_queue_pending_jobs" {
-  name                       = "${var.prefix}-pending-jobs"
-  visibility_timeout_seconds = 60
+# Aqua
+resource "aws_sqs_queue" "aws_sqs_queue_pending_jobs_aqua" {
+  name                       = "${var.prefix}-pending-jobs-aqua.fifo"
+  visibility_timeout_seconds = 5
   sqs_managed_sse_enabled    = true
+  fifo_queue                 = true
 }
 
-resource "aws_sqs_queue_policy" "aws_sqs_queue_policy_pending_jobs" {
-  queue_url = aws_sqs_queue.aws_sqs_queue_pending_jobs.id
+resource "aws_sqs_queue_policy" "aws_sqs_queue_policy_pending_jobs_aqua" {
+  queue_url = aws_sqs_queue.aws_sqs_queue_pending_jobs_aqua.id
   policy = jsonencode({
     "Version" : "2008-10-17",
     "Id" : "__default_policy_ID",
@@ -48,7 +50,65 @@ resource "aws_sqs_queue_policy" "aws_sqs_queue_policy_pending_jobs" {
         "Action" : [
           "SQS:*"
         ],
-        "Resource" : "${aws_sqs_queue.aws_sqs_queue_pending_jobs.arn}"
+        "Resource" : "${aws_sqs_queue.aws_sqs_queue_pending_jobs_aqua.arn}"
+      }
+    ]
+  })
+}
+
+# Terra
+resource "aws_sqs_queue" "aws_sqs_queue_pending_jobs_terra" {
+  name                       = "${var.prefix}-pending-jobs-terra.fifo"
+  visibility_timeout_seconds = 5
+  sqs_managed_sse_enabled    = true
+  fifo_queue                 = true
+}
+
+resource "aws_sqs_queue_policy" "aws_sqs_queue_policy_pending_jobs_terra" {
+  queue_url = aws_sqs_queue.aws_sqs_queue_pending_jobs_terra.id
+  policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Id" : "__default_policy_ID",
+    "Statement" : [
+      {
+        "Sid" : "__owner_statement",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "${local.account_id}"
+        },
+        "Action" : [
+          "SQS:*"
+        ],
+        "Resource" : "${aws_sqs_queue.aws_sqs_queue_pending_jobs_terra.arn}"
+      }
+    ]
+  })
+}
+
+# Viirs
+resource "aws_sqs_queue" "aws_sqs_queue_pending_jobs_viirs" {
+  name                       = "${var.prefix}-pending-jobs-viirs.fifo"
+  visibility_timeout_seconds = 5
+  sqs_managed_sse_enabled    = true
+  fifo_queue                 = true
+}
+
+resource "aws_sqs_queue_policy" "aws_sqs_queue_policy_pending_jobs_viirs" {
+  queue_url = aws_sqs_queue.aws_sqs_queue_pending_jobs_viirs.id
+  policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Id" : "__default_policy_ID",
+    "Statement" : [
+      {
+        "Sid" : "__owner_statement",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "${local.account_id}"
+        },
+        "Action" : [
+          "SQS:*"
+        ],
+        "Resource" : "${aws_sqs_queue.aws_sqs_queue_pending_jobs_viirs.arn}"
       }
     ]
   })
