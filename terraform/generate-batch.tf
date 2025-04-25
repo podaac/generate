ecddef8ae3149648d13c4e4cd0b4d3104a34927f
Hong-Kit Randy Yeung <hong-kit.r.yeung@jpl.nasa.gov>
2025-04-24 19:01:42 -0500

    rename compute for a quick sec before renaming back

--- terraform/generate-batch.tf
+++ terraform/generate-batch.tf
@@ -1,7 +1,7 @@
 # MODIS Aqua
 # Compute Environment
 resource "aws_batch_compute_environment" "generate_aqua" {
-  compute_environment_name = "${var.prefix}-aqua"
+  compute_environment_name = "${var.prefix}-aqua-tmp"
   compute_resources {
     allocation_strategy = "BEST_FIT_PROGRESSIVE"
     ec2_configuration {
@@ -60,7 +60,7 @@ resource "aws_batch_job_queue" "aqua" {
 # MODIS Terra
 # Compute Environment
 resource "aws_batch_compute_environment" "generate_terra" {
-  compute_environment_name = "${var.prefix}-terra"
+  compute_environment_name = "${var.prefix}-terra-tmp"
   compute_resources {
     allocation_strategy = "BEST_FIT_PROGRESSIVE"
     ec2_configuration {
@@ -119,7 +119,7 @@ resource "aws_batch_job_queue" "terra" {
 # VIIRS
 # Compute Environment
 resource "aws_batch_compute_environment" "generate_viirs" {
-  compute_environment_name = "${var.prefix}-viirs"
+  compute_environment_name = "${var.prefix}-viirs-tmp"
   compute_resources {
     allocation_strategy = "BEST_FIT_PROGRESSIVE"
     ec2_configuration {
@@ -178,7 +178,7 @@ resource "aws_batch_job_queue" "viirs" {
 # JPSS1
 # Compute Environment
 resource "aws_batch_compute_environment" "generate_jpss1" {
-  compute_environment_name = "${var.prefix}-jpss1"
+  compute_environment_name = "${var.prefix}-jpss1-tmp"
   compute_resources {
     allocation_strategy = "BEST_FIT_PROGRESSIVE"
     ec2_configuration {
