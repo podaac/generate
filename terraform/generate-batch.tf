622e02951366a63e7bdae2582a7f0cf1da9baee6
Simon Khan <simonhkhan@users.noreply.github.com>
2025-07-14 07:31:17 -0700

    Update EC2 to Fargate for AWS Batch (#26)
    * Updates batch, iam, and sqs config for batch to run Fargate jobs instead of EC2
    
    - New IAM role needs to be created to allow AWS Batch to run Fargate jobs
    - SQS contains queue definition for JPSS1
    - Batch is updated to include resource definitions for Fargate
    
    * Removed lifecycle args from EC2 compute environment
    
    - Added Fargate resource for JPSS1 dataset
    
    * Removed lifecycle policy for dataset

--- terraform/generate-batch.tf
+++ terraform/generate-batch.tf
@@ -29,9 +29,27 @@ resource "aws_batch_compute_environment" "generate_aqua" {
     aws_iam_role_policy_attachment.aws_batch_service_role_policy_attach
   ]
 
-  lifecycle {
-    create_before_destroy = true
+}
+
+# Fargate Compute Environment
+resource "aws_batch_compute_environment" "generate_aqua_fargate" {
+  compute_environment_name = "${var.prefix}-aqua-fargate"
+  compute_resources {
+    type               = "FARGATE"
+    max_vcpus          = 128
+    subnets            = data.aws_subnets.private_application_subnets.ids
+    security_group_ids = data.aws_security_groups.vpc_default_sg.ids
+    # assign_public_ip = true
   }
+  type                     = "MANAGED"
+  state                    = "ENABLED"
+  service_role             = aws_iam_role.aws_batch_service_role.arn
+
+  depends_on = [
+    aws_iam_role.aws_batch_service_role,
+    aws_iam_policy.batch_service_role_policy,
+    aws_iam_role_policy_attachment.aws_batch_service_role_policy_attach
+  ]
 }
 
 # Scheduling Policy
@@ -52,7 +70,7 @@ resource "aws_batch_job_queue" "aqua" {
   priority              = 10
   compute_environment_order {
     order = 1
-    compute_environment = aws_batch_compute_environment.generate_aqua.arn
+    compute_environment = aws_batch_compute_environment.generate_aqua_fargate.arn
   }
   scheduling_policy_arn = aws_batch_scheduling_policy.generate_aqua.arn
 }
@@ -88,9 +106,27 @@ resource "aws_batch_compute_environment" "generate_terra" {
     aws_iam_role_policy_attachment.aws_batch_service_role_policy_attach
   ]
 
-  lifecycle {
-    create_before_destroy = true
+}
+
+# Fargate Compute Environment
+resource "aws_batch_compute_environment" "generate_terra_fargate" {
+  compute_environment_name = "${var.prefix}-terra-fargate"
+  compute_resources {
+    type               = "FARGATE"
+    max_vcpus          = 128
+    subnets            = data.aws_subnets.private_application_subnets.ids
+    security_group_ids = data.aws_security_groups.vpc_default_sg.ids
+    # assign_public_ip = true
   }
+  type                     = "MANAGED"
+  state                    = "ENABLED"
+  service_role             = aws_iam_role.aws_batch_service_role.arn
+
+  depends_on = [
+    aws_iam_role.aws_batch_service_role,
+    aws_iam_policy.batch_service_role_policy,
+    aws_iam_role_policy_attachment.aws_batch_service_role_policy_attach
+  ]
 }
 
 # Scheduling Policy
@@ -111,7 +147,7 @@ resource "aws_batch_job_queue" "terra" {
   priority              = 10
   compute_environment_order {
     order = 1
-    compute_environment = aws_batch_compute_environment.generate_terra.arn
+    compute_environment = aws_batch_compute_environment.generate_terra_fargate.arn
   }
   scheduling_policy_arn = aws_batch_scheduling_policy.generate_terra.arn
 }
@@ -147,9 +183,27 @@ resource "aws_batch_compute_environment" "generate_viirs" {
     aws_iam_role_policy_attachment.aws_batch_service_role_policy_attach
   ]
 
-  lifecycle {
-    create_before_destroy = true
+}
+
+# Fargate Compute Environment
+resource "aws_batch_compute_environment" "generate_viirs_fargate" {
+  compute_environment_name = "${var.prefix}-viirs-fargate"
+  compute_resources {
+    type               = "FARGATE"
+    max_vcpus          = 128
+    subnets            = data.aws_subnets.private_application_subnets.ids
+    security_group_ids = data.aws_security_groups.vpc_default_sg.ids
+    # assign_public_ip = true
   }
+  type                     = "MANAGED"
+  state                    = "ENABLED"
+  service_role             = aws_iam_role.aws_batch_service_role.arn
+
+  depends_on = [
+    aws_iam_role.aws_batch_service_role,
+    aws_iam_policy.batch_service_role_policy,
+    aws_iam_role_policy_attachment.aws_batch_service_role_policy_attach
+  ]
 }
 
 # Scheduling Policy
@@ -170,7 +224,7 @@ resource "aws_batch_job_queue" "viirs" {
   priority              = 10
   compute_environment_order {
     order = 1
-    compute_environment = aws_batch_compute_environment.generate_viirs.arn
+    compute_environment = aws_batch_compute_environment.generate_viirs_fargate.arn
   }
   scheduling_policy_arn = aws_batch_scheduling_policy.generate_viirs.arn
 }
@@ -206,9 +260,27 @@ resource "aws_batch_compute_environment" "generate_jpss1" {
     aws_iam_role_policy_attachment.aws_batch_service_role_policy_attach
   ]
 
-  lifecycle {
-    create_before_destroy = true
+}
+
+# Fargate Compute Environment
+resource "aws_batch_compute_environment" "generate_jpss1_fargate" {
+  compute_environment_name = "${var.prefix}-jpss1-fargate"
+  compute_resources {
+    type               = "FARGATE"
+    max_vcpus          = 128
+    subnets            = data.aws_subnets.private_application_subnets.ids
+    security_group_ids = data.aws_security_groups.vpc_default_sg.ids
+    # assign_public_ip = true
   }
+  type                     = "MANAGED"
+  state                    = "ENABLED"
+  service_role             = aws_iam_role.aws_batch_service_role.arn
+
+  depends_on = [
+    aws_iam_role.aws_batch_service_role,
+    aws_iam_policy.batch_service_role_policy,
+    aws_iam_role_policy_attachment.aws_batch_service_role_policy_attach
+  ]
 }
 
 # Scheduling Policy
@@ -229,7 +301,7 @@ resource "aws_batch_job_queue" "jpss1" {
   priority              = 10
   compute_environment_order {
     order = 1
-    compute_environment = aws_batch_compute_environment.generate_jpss1.arn
+    compute_environment = aws_batch_compute_environment.generate_jpss1_fargate.arn
   }
   scheduling_policy_arn = aws_batch_scheduling_policy.generate_jpss1.arn
 }
